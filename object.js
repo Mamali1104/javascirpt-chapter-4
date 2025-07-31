f46b9ca0aaa9e756640e9812b744139c9af3d65f
Mamali sahoo <mamalisahoo1104@gmail.com>
2025-07-31 20:05:25 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,92 @@
+const student={
+    name:"Mama",
+    age:"20",
+    mark:"9.4",
+    city:"Delhi"
+};
+
+
+
+
+
+let stu=["mama",20,9.4];
+let item={
+    price:199.20,
+    discount:50,
+    color:["red","pink"]
+};
+
+let post={
+    username:"@blackforest",
+    content:"educational",
+    repost:90,
+    likes:900,
+    tags:["@dudu","@bubu"]
+};
+let prop="repost";
+post[prop];
+//post.prop         it doesn't store variables..
+
+
+
+
+
+
+
+
+const obj={
+    1:"a",      //obj.1 it does't convert to string undefined error
+    2:"b",
+    3:"c",
+    true:"d",       //obj.true  it convert it to string give output..
+    null:"e"
+};                      //we can access as obj[1];
+
+  
+
+
+
+
+
+
+//object of obj
+
+
+const classInfo={
+    Mama:{
+        grade:9.5,
+        city:"bbsr"
+    },
+    Dudu :{
+        grade:9.0,
+        city:"bdk"
+    }
+};
+
+
+
+
+//array of obj
+
+
+ const studentinfo=[
+    {
+        name:"mama",
+        grade:9.5,
+        city:"bbsr"
+    },
+    {
+       
+        name:"dudu",
+        grade:9.0,
+        city:"bdk"
+    }
+
+];
+
+
+let step1 = Math.random();
+step2=step1*10;
+step3=Math.floor(step1);
+
+let random=Math.floor(Math.random() *100 +1);
